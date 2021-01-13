db643158cc210771eb9eb41d4f97159d593278bd
Urkia Pauline <urkia.p@free.fr>
2021-01-13 12:13:41 +0100

    Add kanye quote

--- project/src/Controller/DefaultController.php
+++ project/src/Controller/DefaultController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpClient\HttpClient;
 class DefaultController extends AbstractController
 {
     /**
@@ -13,8 +13,19 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
+        var_dump($this->getKanyeQuote());
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
+    } 
+    
+    private function getKanyeQuote(): String
+    {
+
+        $apiUrl = "https://api.kanye.rest/";
+        $httpClient = HttpClient::create();
+        $response = $httpClient->request('GET', $apiUrl);
+        $content = $response->toArray();
+        return $content['quote'];
     }
 }
